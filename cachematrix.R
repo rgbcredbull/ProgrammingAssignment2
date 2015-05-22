a8890583cee66faf1f4001d0aa1c86835ad6efd5
rgbcredbull <billmcgeeney@gmail.com>
2015-05-21 20:20:50 -0400

    amount.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix builds new makeCacheMatrix object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  ##declares m as null. 
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  ##set declares m null at the environmental overiding parent declaration.
+  ##sets input as x.
+  get<-function(){
+    x
+  }
+  ##get function returns the unaltered value of x.
+  setsolve<-function(solve){
+    m<<-solve
+  }
+  ##setsolve sets m as the cache file.
+  getsolve<-function(){
+    m
+  }
+  ##getsolve returns the cache file.
+  
+  list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve evaluates the makeCacheMatrix object.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getsolve()
+  ##returns the cache value.
+  if(!is.null(m)){
+    message("getting catched data")
+    return(m)
+  }
+  ##if there is a cache value, returns cache data.
+  data<-x$get()
+  ##returns current value for x (unlatered data).
+  m<-solve(data,...)
+  ##solves the matrix.
+  x$setsolve(m)
+  ##sets the cache value for m.
+  m
+  ## Return a matrix that is the inverse of 'x'.
 }
+
